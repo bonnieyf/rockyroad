8c2e488eabdcd2462d8e3dbdcabe430f81dfe438
bonnie <kusasen@gmail.com>
2023-01-30 22:49:29 +0800

    first commit

---
+++ js/loaded.js
@@ -0,0 +1,46 @@
+$(function(){
+
+    let loaded = false;
+    let loadedCnt = 0;
+    let loadStatus = 0;
+    let loadPercent = 0;
+    const allImgs = document.querySelectorAll('img').length;
+    const progressCnt = document.getElementById('progress-count');
+    const imgLoad = imagesLoaded('img');
+
+    let tlProgress = gsap.timeline({
+    onUpdate: () => {
+        loadPercent = Math.floor(tlProgress.progress() * 100);
+        progressCnt.style.width = loadPercent + '%';
+
+        if(loadPercent == '100'){
+            loaded = true;
+        }
+    },
+    onComplete: () => {
+        if(loaded){
+            let tlComplete = gsap.timeline()
+            .to('#loading', {'opacity': 0, duration: 0.7 ,ease: Power2.easeOut})
+            .to('#loading', {'display': 'none'});
+
+            let initAni = gsap.timeline()
+                .set('#home .car', {'opacity': 0 ,y: -200})
+                .set('#home .big-logo', {'opacity': 0,y: -500 ,scale:0 ,rotation: '100deg'})
+                .to('#home .car', {'opacity': 1, duration: 0.8 ,y: 0 ,ease: Back.easeOut})
+                .to('#home .big-logo', {'opacity': 1, duration: 0.8 ,y: 0 ,scale:1 ,rotation:'0deg' ,ease: Back.easeOut})
+        }
+    }
+    });
+
+    tlProgress.to('#progress-bar', {x: 0})
+
+    if (allImgs > 0) {
+    imgLoad.on( 'progress', function( instance, image ) {
+        loadedCnt++;
+        loadStatus = loadedCnt/allImgs;
+        gsap.to(tlProgress, {progress:loadStatus})
+    });
+    };
+
+    
+});
---
+++ js/page.js
@@ -0,0 +1,24 @@
+$(function(){
+
+    $(".btn-navigation").click(function(e){
+        e.preventDefault();
+        let menu = $("#navigation-box");
+        menu.toggleClass('show');
+        if(menu.hasClass('show')){
+            gsap.to(menu, { duration: .8 ,display:'block',x: 0,opacity: 1, ease: "power2.out"});
+        }else{
+            gsap.to(menu, { duration: .5 ,display:'none',x: 200 ,opacity: 0, ease: "power2.out"});
+        }
+    });
+
+    $('.sambo-nav a').click(function(e){
+        e.preventDefault();
+        let showItem = $(this).attr('href');
+        $('.sambo-nav a.active').removeClass('active');
+        $(this).addClass('active');
+        $('.sambo-slide').css({display:'none',opacity: 0});
+        $(showItem).css({display:'block',opacity: 1});
+
+    });
+    
+});
