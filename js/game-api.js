d3b15167de65345181a3eac2528475457afb3f4d
Bonnie.wang <kusasen@gmail.com>
2023-04-17 21:46:38 +0800

    更新 layout 的畫面 bug

--- js/game-api.js
+++ js/game-api.js
@@ -1,6 +1,11 @@
 const API = 'http://rockyroad.02580.me/api/questions.json';
+const RESULT_API = 'http://rockyroad.02580.me/';
+let dataURL;
 let gameInitCount = 3; // 遊戲開始前的倒數
-let totalScore = 0; // 總分計算
+let totalScore = 0;// 總分計算
+let scoreList = [
+    0,0,0,0,0,0
+];
 let isVideoMuted = false; // 影片是否靜音
 let isAudioMuted = false; // 背景音是否靜音
 let isPlayIntroAnimation = true; // 是否撥放導引動畫
@@ -11,7 +16,8 @@ let volumeList = [
     0.8,
     1
 ];
-let volumeCount = 1;
+let userNumber;
+let volumeCount = 2;
 let maxVolumeCount = volumeList.length - 1;
 let currentAudioVolume = volumeList[volumeCount]; // 背景音音量: 0.2 /0.4 / 0.6 / 0.8 / 1
 let topicLevel = 0; // 從第幾關開始
@@ -38,6 +44,8 @@ let transList = {
             {prev: "2-4", next:"3-0",video_code: "2-4_3-0"}
         ]
     };
+
+
 
 $(function(){
     let $game_topice =  $('#game-animation .topic');
@@ -134,18 +142,23 @@ $(function(){
         let index = $(this).parent().index();
         $('.music-menu li.active,.music-sample div.active').removeClass('active');
         $(this).parent().addClass('active');
-
+        scoreList = [
+            0,0,0,0,0,0
+        ];
         // 選擇音樂，並加入一開始的暴怒分數
         switch(index){
             case 1:
-                totalScore +=3
+                scoreList[3] = 3
                 break;
             case 2:
-                totalScore +=2
+                scoreList[2] = 2
                 break;
+
+            default:
+                scoreList[0] = 0
         }
 
-        console.log('** 初始分數: '+totalScore + '**');
+        console.log('** 初始分數: '+scoreList + '**');
 
         $('.music-sample div').eq(index).addClass('active');
         $('.background-music').empty();
@@ -206,7 +219,11 @@ $(function(){
         });
     });
 
-    
+    $(document).on('click','.btn-share.btn-link',function(e){
+        e.preventDefault();
+        share_fb();
+    });
+
     //選擇每一題選項後
     $(document).on('click','.choose-myoption',function(e){
         e.preventDefault();
@@ -221,19 +238,21 @@ $(function(){
 
         $('.game-popup .inner-textarea').empty();
 
-        totalScore+=myscore;
+        let oldScroe = scoreList[myscore];
+        scoreList[myscore] = oldScroe+myscore;
 
-        if(_isColor == 'red'){
+        if(_isExtra && _isColor == 'red'){
+            $('#game-oops .inner-textarea').append(topicList[topicLevel].options[idx].extra);
+            $('#game-oops').addClass('active');
+            $('.overlay').fadeIn();
+            $('.btn-next-more').attr('data-file',myresult);
+            
+        }else if(_isColor == 'red'){
             $('#game-warning .inner-textarea').append(topicList[topicLevel].options[idx].message);
             $('#game-warning').addClass('active');
             $('.overlay').fadeIn();
             $('.btn-next-more').attr('data-file',myresult);
 
-        }else if(_isExtra){
-            $('#game-oops .inner-textarea').append(topicList[topicLevel].options[idx].extra);
-            $('#game-oops').addClass('active');
-            $('.overlay').fadeIn();
-            $('.btn-next-more').attr('data-file',myresult);
         }else{
 
             fadeOutTopic();
@@ -249,7 +268,7 @@ $(function(){
                 handleTopicLevel();
             });
         }
-
+        console.log(`%c======= 總分 ${scoreList} 分======`, 'color: red');
         console.log(`%c======= 點選此選項增加 ${myscore} 分======`, 'color: red');
 
     });
@@ -273,14 +292,151 @@ $(function(){
             handleTopicLevel();
         });
     });
+
+
+
+    
 });
 
+function share_fb() {
+    let userName = $('input[name="result-user-name"]').val();
+    console.log(userName);
+    if(userName.length == ''){
+        alert('請填寫姓名資料!');
+    }
 
+    console.log(userNumber);
+    $.ajax({
+        url: RESULT_API+'scores/'+userNumber,
+        method:"PUT",
+        data: {
+            score:{
+                name: userName
+            }
+        },
+        success: function(res){
+          console.log(res);
+        }
+    });
 
+	var url = encodeURI('http://rockyroad.02580.me/');
+    var img= encodeURI('http://rockyroad.02580.me/images/meta.png');
+    var totalurl = encodeURIComponent(url+'?img='+img);
+  	var share_link = "https://www.facebook.com/sharer/sharer.php?u="+totalurl;
+  	window.open(share_link,'_blank');
+}
+
+function capture() {
+    html2canvas(document.querySelector("#result-licencebox")).then(function (canvas) {
+        var extra_canvas = document.createElement("canvas");
+        let c_width = $('#result-licencebox').width();
+        let c_height = $('#result-licencebox').height();
+            extra_canvas.setAttribute('width',c_width);
+            extra_canvas.setAttribute('height',c_height);
+            var ctx = extra_canvas.getContext('2d');
+            ctx.drawImage(canvas,0,0,canvas.width, canvas.height,0,0,c_width,c_height);
+            dataURL = extra_canvas.toDataURL();
+      });
+  }
 function handleTopicLevel(){
 
     if(topicLevel === topicMaxLenght){
+
+        let currentScore = {
+            score: {
+                "good_boy":scoreList[0],
+                "bad_1":scoreList[1],
+                "bad_2":scoreList[2],
+                "bad_3":scoreList[3],
+                "bad_4":scoreList[4],
+                "bad_5":scoreList[5],
+                
+            }
+        }
+
+        $.ajax({
+            url: RESULT_API+'scores.json',
+            method:"POST",
+            data: currentScore,
+            success: function(res){
+              console.log('ajax result:');
+              userNumber = res.id;
+              console.log(res.id)
+            }
+        });
+        
         $('#game-result').fadeIn();
+
+        
+
+        let maxScore = Math.max(...scoreList);
+        let maxScoreIndex = scoreList.indexOf(maxScore);
+        $('#result-licence').attr('src','images/result_'+ maxScoreIndex +'_licence.svg');
+
+        let resultConfig = {
+            type : 'radar',
+            plot : {
+                aspect : 'area',
+                stacked: true,
+            },
+            scaleV: {
+                values: "0:15:4",
+                labels: [ '', '', '', '', '' ],
+                item: {
+                    "font-color": "orange",
+                    "font-family": "Georgia",
+                    "font-size": 12,
+                    "font-weight": "bold",
+                    "font-style": "italic"
+                },
+                refLine: {
+                    'line-color': "none"
+                },
+                "guide": {
+                    "line-color": "#000",
+                    "line-width": 2,
+                    "line-style": "solid",
+                }
+                },
+            scaleK : {
+                labels : ['咄咄逼人型','老子最屌型','就是很想贏型', '要你好看型', '下車PK型'],
+                item : {
+                fontColor : '#383738',
+                padding : '5 10',
+                fontSize: '16px'
+                },
+                tick: {
+                lineWidth: 0,
+                placement: "outer"
+                },
+                
+                guide : {
+                lineColor : "#000",
+                lineStyle : 'solid',
+                lineWidth: 2,
+                backgroundColor: "#fff #fff"
+                }
+            },
+            series : [
+                {
+                    values: [
+                        scoreList[1],
+                        scoreList[2],
+                        scoreList[3],
+                        scoreList[4],
+                        scoreList[5],
+                    ]
+                }
+            ]
+        };
+
+        zingchart.render({ 
+            id : 'myChart', 
+            data : resultConfig, 
+            height: '100%', 
+            width: '100%' 
+        });
+
         $('#game-animation').hide();
         return false;
     }
